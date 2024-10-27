0a3b24b2d3f85948db934ca24344e0cc4d485353
Was403 <vladimirovichdanil@bk.ru>
2024-10-27 23:29:00 +0300

    input btn & clcklistener

--- app/src/main/java/com/dk/demo/MainActivity.kt
+++ app/src/main/java/com/dk/demo/MainActivity.kt
@@ -1,11 +1,17 @@
 package com.dk.demo
 
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.ImageButton
+import android.widget.TextView
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
@@ -18,6 +24,13 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        val btn_input: ImageButton = findViewById(R.id.btn_input)
+        val text_output: TextView = findViewById(R.id.textOutput)
+        btn_input.setOnClickListener{
+            text_output.setText(R.string.app_name)
+            text_output.visibility = View.VISIBLE
+        }
     }
+
 }
 
