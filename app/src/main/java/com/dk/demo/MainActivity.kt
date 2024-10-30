8357b02c2068dedd7aad402fae415283fca495c0
Was403 <vladimirovichdanil@bk.ru>
2024-10-30 23:09:10 +0300

    MaterialalertDialog work

--- app/src/main/java/com/dk/demo/MainActivity.kt
+++ app/src/main/java/com/dk/demo/MainActivity.kt
@@ -3,28 +3,36 @@ package com.dk.demo
 import android.animation.AnimatorSet
 import android.animation.ObjectAnimator
 import android.os.Bundle
-import android.view.LayoutInflater
 import android.view.View
+import android.view.ViewGroup
 import android.widget.Button
+import android.widget.EditText
 import android.widget.ImageButton
 import android.widget.ImageView
 import android.widget.LinearLayout
-import android.widget.RelativeLayout
 import android.widget.TextView
 import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.compose.material3.AlertDialog
-import com.dk.demo.ui.theme.DemoTheme
+import androidx.constraintlayout.widget.ConstraintLayout
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import com.google.android.material.textfield.TextInputEditText
 
 class MainActivity : ComponentActivity() {
+
+    private lateinit var backgroundPink: ConstraintLayout
+    private var originalWidth: Int = 0
+    private var originalHeight: Int = 0
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
         // Мои переменные
+        backgroundPink = findViewById(R.id.backgroundPink)
+        originalWidth = backgroundPink.layoutParams.width
+        originalHeight = backgroundPink.layoutParams.height
         val btn_input: ImageButton = findViewById(R.id.btn_input)
         val img_nav_main: ImageView = findViewById(R.id.img_nav_main)
         val img_nav_result: ImageView = findViewById(R.id.img_nav_result)
@@ -32,9 +40,10 @@ class MainActivity : ComponentActivity() {
         val btn_nav_main: Button = findViewById(R.id.nav_item1)
         val btn_nav_result: Button = findViewById(R.id.nav_item2)
         val btn_nav_settings: Button = findViewById(R.id.nav_item3)
-        var sample_text = ""
+
         val text_output: TextView = findViewById(R.id.textOutput)
         val text_welcome: TextView = findViewById(R.id.textWelcome)
+        val textUnderSticker: TextView = findViewById(R.id.textUnderSticker)
 
         val layout_results: LinearLayout = findViewById(R.id.layout_results_nav)
         val layout_settings: LinearLayout = findViewById(R.id.layout_settings_nav)
@@ -47,50 +56,91 @@ class MainActivity : ComponentActivity() {
 
             activeLayout.setBackgroundResource(R.drawable.shape_rounded_choosen_nav)
         }
-        fun showAlertDialogWithInput() {
-            val builder = MaterialAlertDialogBuilder(this)
-                .setTitle("Введите текст")
-                .setView(R.layout.dialog_layout)  // Используем разметку для ввода текста
-                .setPositiveButton("ОК") { dialog, which ->
-                    val dialog_input: TextInputEditText = findViewById(R.id.dialog_input)
-                    val text = dialog_input?.text.toString()
-                    Toast.makeText(this, "Введенный текст: $text", Toast.LENGTH_SHORT).show()
-                }
-                .setNegativeButton("Отмена") { dialog, which ->
-                    dialog.cancel()
-                }
-                .show()
-        }
         // Кнопки
         btn_input.setOnClickListener{
-            text_output.setText(R.string.app_name)
-            text_output.visibility = View.VISIBLE
+            //text_output.setText(R.string.app_name)
+            //text_output.visibility = View.VISIBLE
+            // showAlertDialogWithInput()
             showAlertDialogWithInput()
         }
-        // Navigation buttons
+        // Кнопки навигации
         btn_nav_result.setOnClickListener{
             setActiveLayout(layout_results)
+            expandbackgroundPink()
             animateButtonPress(btn_nav_result)
             text_welcome.visibility = View.GONE
+            textUnderSticker.visibility = View.GONE
             img_nav_result.setImageResource(R.drawable.ic_bookmark_filled)
             img_nav_main.setImageResource(R.drawable.ic_chat_bubble)
             img_nav_settings.setImageResource(R.drawable.ic_settings)
         }
         btn_nav_main.setOnClickListener{
             setActiveLayout(layout_main)
+            collapsebackgroundPink()
             text_welcome.visibility = View.VISIBLE
+            textUnderSticker.visibility = View.VISIBLE
             img_nav_main.setImageResource(R.drawable.ic_chat_bubble)
             img_nav_result.setImageResource(R.drawable.ic_results)
             img_nav_settings.setImageResource(R.drawable.ic_settings)
         }
         btn_nav_settings.setOnClickListener{
             setActiveLayout(layout_settings)
+            expandbackgroundPink()
             text_welcome.visibility = View.GONE
+            textUnderSticker.visibility = View.GONE
             img_nav_settings.setImageResource(R.drawable.ic_settings_filled)
             img_nav_result.setImageResource(R.drawable.ic_results)
             img_nav_main.setImageResource(R.drawable.ic_chat_bubble)
         }
 
+    }
+    private fun expandbackgroundPink() {
+        // Возвращает в исходную розовый фон
+        val params = backgroundPink.layoutParams as ViewGroup.LayoutParams
+        params.width = ViewGroup.LayoutParams.MATCH_PARENT
+        params.height = ViewGroup.LayoutParams.MATCH_PARENT
+        backgroundPink.layoutParams = params
+
+        // Анимация
+        val animator = ObjectAnimator.ofFloat(backgroundPink, "scaleY", backgroundPink.scaleY, 1f)
+        animator.duration = 800 // Длительность анимации в миллисекундах
+        animator.start()
+    }
+    private fun collapsebackgroundPink() {
+        // Раскрывает розовый фон
+        val params = backgroundPink.layoutParams as ViewGroup.LayoutParams
+        params.width = originalWidth
+        params.height = originalHeight
+        backgroundPink.layoutParams = params
+
+        // Анимация
+        val animator = ObjectAnimator.ofFloat(backgroundPink, "scaleY", 1f, backgroundPink.scaleY)
+        animator.duration = 800 // Длительность анимации в миллисекундах
+        animator.start()
+    }
+
+    private fun showAlertDialogWithInput() {
+        val view = View.inflate(this, R.layout.dialog_layout, null)
+        val builder = MaterialAlertDialogBuilder(this)
+            .setTitle("Введите текст")
+            .setView(view)
+            .setPositiveButton("ОК") { dialog, which ->
+                val input = view.findViewById<EditText>(R.id.dialog_input)
+                val text = input?.text.toString()
+                Toast.makeText(this, "Введенный текст: $text", Toast.LENGTH_SHORT).show()
+                handleInputText(text)
+            }
+            .setNegativeButton("Отмена") { dialog, which ->
+                dialog.cancel()
+            }
+            .show()
+    }
+
+    private fun handleInputText(text: String) {
+        // Здесь происходит обработка введенного текста
+        // Например, можно вывести его в TextView, отправить на сервер,
+        // сохранить в базу данных и т. д.
+        // ...
     }
     private fun animateButtonPress(button: Button) {
         val scaleDown = ObjectAnimator.ofFloat(button, "scaleX", 1f, 0.95f)
